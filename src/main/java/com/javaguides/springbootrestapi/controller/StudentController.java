a0e10ac18a2d6c1f198ed5ae89bba29300678aa7
seedevk8s <seedevk8s@gmail.com>
2024-04-08 05:47:43 +0000

    POST REST API - @PostMapping, @RequestBody 추가

--- src/main/java/com/javaguides/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/javaguides/springbootrestapi/controller/StudentController.java
@@ -1,8 +1,12 @@
 package com.javaguides.springbootrestapi.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaguides.springbootrestapi.bean.Student;
@@ -61,6 +65,23 @@ public class StudentController {
                                             @RequestParam String firstName,
                                             @RequestParam String lastName) {
         return new Student(id, firstName, lastName);
+    }    
+
+    @PostMapping("students/create")
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    @PostMapping("students2/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent2(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }    
 
 }
