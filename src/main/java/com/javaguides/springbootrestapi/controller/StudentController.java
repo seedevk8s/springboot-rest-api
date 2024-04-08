bc3b814b273d7af65d3e8eae627917b9d0baaeb5
seedevk8s <seedevk8s@gmail.com>
2024-04-08 08:53:38 +0000

    각 엔드 포인트의 리턴타입을 HTTP 응답의  ResponseEntity 타입으로 변경하기

--- src/main/java/com/javaguides/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/javaguides/springbootrestapi/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.javaguides.springbootrestapi.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -99,4 +100,77 @@ public class StudentController {
         return "Student 삭제 성공함!";
     }
 
+    @GetMapping("student2")
+    public ResponseEntity<Student> getStudent2() {
+        Student student = new Student(1, "hojin", "Chu");
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
+    @GetMapping("student3")
+    public ResponseEntity<Student> getStudent3() {
+        Student student = new Student(1, "hojin", "Chu");
+        return ResponseEntity.ok(student);
+    }
+    
+    @GetMapping("student4")
+    public ResponseEntity<Student> getStudent4() {
+        Student student = new Student(1, "hojin", "Chu");
+        return ResponseEntity.ok()
+                            .header("custom-header", "hojin")
+                            .body(student);
+    }
+
+
+
+
+
+
+    @GetMapping("students2")
+    public ResponseEntity<List<Student>> getStudents2() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "hojin", "chu"));
+        students.add(new Student(2, "les1", "vig1"));
+        students.add(new Student(3, "les2", "vig2"));
+        students.add(new Student(4, "les3", "vig3"));
+        return ResponseEntity.ok(students);
+    }
+ 
+    @GetMapping("students5/{ids}/{first-name}/{last-name}")
+    public ResponseEntity<Student> studentPathVariable5(@PathVariable("ids") int studentId,
+                                        @PathVariable("first-name") String firstName,
+                                        @PathVariable("last-name") String lastName) {
+        Student student = new Student(studentId, firstName, lastName);                                            
+        return ResponseEntity.ok(student);
+    } 
+
+    @GetMapping("students3/query")
+    public ResponseEntity<Student> studentRequestVariable3(@RequestParam int id,
+                                            @RequestParam String firstName,
+                                            @RequestParam String lastName) {
+        Student student = new Student(id, firstName, lastName);                                                
+        return ResponseEntity.ok(student);
+    } 
+
+    
+    @PostMapping("students3/create")
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Student> createStudent3(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
+    } 
+
+    @PutMapping("students2/{id}/update")
+    public ResponseEntity<Student> updateStudent2(@RequestBody Student student, @PathVariable("id") int studentId) {
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return ResponseEntity.ok(student);
+    }
+
+    @DeleteMapping("students2/{id}/delete")
+    public ResponseEntity<String> deleteStudent2(@PathVariable("id") int studentId) {
+        System.out.println(studentId);
+        return ResponseEntity.ok("Student 삭제 성공함!");
+    }
 }
