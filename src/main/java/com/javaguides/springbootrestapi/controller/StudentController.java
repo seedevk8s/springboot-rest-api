e5a78fc297774491f8202a31ba9646718ba15376
seedevk8s <seedevk8s@gmail.com>
2024-04-08 01:35:02 +0000

    Rest api 요청에 리스트 형태로 리턴하는 메서드 추가

--- src/main/java/com/javaguides/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/javaguides/springbootrestapi/controller/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javaguides.springbootrestapi.bean.Student;
+import java.util.*;
 
 @RestController
 public class StudentController {
@@ -14,4 +15,13 @@ public class StudentController {
         return student;
     }
 
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "hojin", "chu"));
+        students.add(new Student(2, "les1", "vig1"));
+        students.add(new Student(3, "les2", "vig2"));
+        students.add(new Student(4, "les3", "vig3"));
+        return students;
+    }
 }
