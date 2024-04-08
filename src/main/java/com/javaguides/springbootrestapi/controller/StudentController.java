508bff6130c16b48c407069f7fb659fa982ec09c
seedevk8s <seedevk8s@gmail.com>
2024-04-08 06:57:36 +0000

    PUT REST API - @PutMapping, @RequestBody 추가

--- src/main/java/com/javaguides/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/javaguides/springbootrestapi/controller/StudentController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -83,5 +84,12 @@ public class StudentController {
         System.out.println(student.getLastName());
         return student;
     }    
+
+    @PutMapping("students/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
 
 }
